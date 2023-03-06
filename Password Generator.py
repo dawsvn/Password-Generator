b30aae85a982ed9ad988aec4be6693257befd1e4
dawsvn <dawson.dillavou@gmail.com>
2023-03-05 18:05:07 -0600

    Password Generator

---
+++ Password Generator.py
@@ -0,0 +1,48 @@
+# Password Generator
+
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9' '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '{', '}', '*',]
+
+print("Welcome to the PyPassword Generator!")
+
+nr_letters= int(input("How many letters would you like in your password?\n"))
+if nr_letters > 52:
+    print('Too many characters for "letters". Try again.')
+    exit()
+
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+if nr_symbols > 12:
+    print('Too many characters for "symbols". Try again.')
+    exit(
+
+    )
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+if nr_numbers > 20:
+    print('Too many characters for "numbers". Try again.')
+    exit()
+
+
+letters_in_pass = random.sample(letters, nr_letters)
+numbers_in_pass = random.sample(numbers, nr_numbers)
+symbols_in_pass = random.sample(symbols, nr_symbols)
+
+password_list = []
+
+for char in range(1, nr_letters + 1):
+  password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+  password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+  password_list += random.choice(numbers)
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
